d30c5d91476e906340d3da4ec8e35a91d49dd693
Isuke Fujiyama <isuke770@gmail.com>
2014-05-27 00:35:37 +0900

    sign in after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Our Walls!"
-      redirect_to @user
+      redirect_to root_path
     else
       render 'new'
     end
