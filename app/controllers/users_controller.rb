709103c73030de5146ffd7bc7e150699ae314b09
Isuke Fujiyama <isuke770@gmail.com>
2014-06-14 21:21:08 +0900

    Fix page transition after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Our Walls!"
-      redirect_to root_path
+      redirect_to @user
     else
       render 'new'
     end
